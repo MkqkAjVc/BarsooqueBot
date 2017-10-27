e30d5f537adb8ee1a8f5d30d1356b89b40e89aa3
MkqkAjVc <33159034+MkqkAjVc@users.noreply.github.com>
2017-10-27 19:14:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,12 @@
-# -*- coding: utf-8 -*-
-import redis
 import os
 import telebot
-# import some_api_lib
-# import ...
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+token = os.environ['BOT_TOKEN']
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+bot = telebot.TeleBot(token)
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+@bot.message_handler()
+def handle_message(m):
+	bot.send_message(m.chat.id, "Хуй")
+
+bot.polling(none_stop=1, interval=0, timeout=100000)
